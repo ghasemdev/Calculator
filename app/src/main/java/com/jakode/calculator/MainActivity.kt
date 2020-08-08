8b612c81c182c894ae68e130c9b100d5c88ef8e2
ghasem shirdel <shirdelghasem79@gmail.com>
2020-08-08 19:59:50 +0430

    init commit

---
+++ app/src/main/java/com/jakode/calculator/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.jakode.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jakode.calculator.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
