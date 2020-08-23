1bba154b84aa6272a8f0be6144fb6148d4cbffc1
ghasem shirdel <shirdelghasem79@gmail.com>
2020-08-23 18:59:32 +0430

    refactoring MainActivity.kt

--- app/src/main/java/com/jakode/calculator/MainActivity.kt
+++ app/src/main/java/com/jakode/calculator/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GestureDetectorCompat
 import androidx.core.view.doOnPreDraw
 import com.jakode.calculator.databinding.ActivityMainBinding
+import com.jakode.calculator.utils.Guidance
+import com.jakode.calculator.utils.Type
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.math.abs
 
@@ -28,27 +30,49 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         mDetector = GestureDetectorCompat(this, MyGestureListener(binding))
 
-        binding.key0.setOnClickListener { binding.calculatorOutput.addItem("0") }
-        binding.key1.setOnClickListener { binding.calculatorOutput.addItem("1") }
-        binding.key2.setOnClickListener { binding.calculatorOutput.addItem("2") }
-        binding.key3.setOnClickListener { binding.calculatorOutput.addItem("3") }
-        binding.key4.setOnClickListener { binding.calculatorOutput.addItem("4") }
-        binding.key5.setOnClickListener { binding.calculatorOutput.addItem("5") }
-        binding.key6.setOnClickListener { binding.calculatorOutput.addItem("6") }
-        binding.key7.setOnClickListener { binding.calculatorOutput.addItem("7") }
-        binding.key8.setOnClickListener { binding.calculatorOutput.addItem("8") }
-        binding.key9.setOnClickListener { binding.calculatorOutput.addItem("9") }
-
-        binding.keyClear.setOnClickListener { binding.calculatorOutput.clear() }
-        binding.keyEqual.setOnClickListener { binding.calculatorOutput.solve() }
-
-        binding.keySum.setOnClickListener { binding.calculatorOutput.addItem("+") }
-        binding.keySub.setOnClickListener { binding.calculatorOutput.addItem("–") }
-        binding.keyMul.setOnClickListener { binding.calculatorOutput.addItem("×") }
-        binding.keyDiv.setOnClickListener { binding.calculatorOutput.addItem("÷") }
-        binding.keyPercentage.setOnClickListener { binding.calculatorOutput.addItem("%") }
+        // onClickListener
+        numberKey()
+        operatorKey()
+        bracesKey()
+        otherKey()
 
         // guidance for clear calculate
+        guide()
+    }
+
+    private fun numberKey() {
+        binding.key0.setOnClickListener { binding.calculatorOutput.addItem("0", Type.Number) }
+        binding.key1.setOnClickListener { binding.calculatorOutput.addItem("1", Type.Number) }
+        binding.key2.setOnClickListener { binding.calculatorOutput.addItem("2", Type.Number) }
+        binding.key3.setOnClickListener { binding.calculatorOutput.addItem("3", Type.Number) }
+        binding.key4.setOnClickListener { binding.calculatorOutput.addItem("4", Type.Number) }
+        binding.key5.setOnClickListener { binding.calculatorOutput.addItem("5", Type.Number) }
+        binding.key6.setOnClickListener { binding.calculatorOutput.addItem("6", Type.Number) }
+        binding.key7.setOnClickListener { binding.calculatorOutput.addItem("7", Type.Number) }
+        binding.key8.setOnClickListener { binding.calculatorOutput.addItem("8", Type.Number) }
+        binding.key9.setOnClickListener { binding.calculatorOutput.addItem("9", Type.Number) }
+    }
+
+    private fun operatorKey() {
+        binding.keySum.setOnClickListener { binding.calculatorOutput.addItem("+", Type.Operator) }
+        binding.keySub.setOnClickListener { binding.calculatorOutput.addItem("-", Type.Operator) }
+        binding.keyMul.setOnClickListener { binding.calculatorOutput.addItem("*", Type.Operator) }
+        binding.keyDiv.setOnClickListener { binding.calculatorOutput.addItem("/", Type.Operator) }
+        binding.keyPercentage.setOnClickListener { binding.calculatorOutput.addItem("%", Type.Operator) }
+    }
+
+    private fun bracesKey() {
+        binding.keyBraces.setOnClickListener { binding.calculatorOutput.addItem("(", Type.Operator) }
+        binding.keyNegative.setOnClickListener { binding.calculatorOutput.addItem("(-", Type.Operator) }
+    }
+
+    private fun otherKey() {
+        binding.keyClear.setOnClickListener { binding.calculatorOutput.clear() }
+        binding.keyEqual.setOnClickListener { binding.calculatorOutput.solve() }
+        binding.keyDecimal.setOnClickListener { binding.calculatorOutput.addItem(".", Type.Operator) }
+    }
+
+    private fun guide() {
         guidance = Guidance(this)
         binding.root.doOnPreDraw {
             if (guidance!!.first) {
